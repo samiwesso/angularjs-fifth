727eb314ad81e1bf93f11b4e66d464f7e90ea65f
Sami Wesso <kerpigli@hotmail.com>
2019-05-01 12:12:08 +0200

    angularlastwork

---
+++ restapi/server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const app = require('./app');
+const port = process.env.SERVER_PORT || 3001;
+//const port = process.env.port || 5000;
+
+const db = require("mongoose");
+
+// WEB SERVER
+http.createServer(app).listen(port, function() {
+    console.log(`WEB SERVER online on port ${port}.`);
+})
+
+// MONGO DB
+db.connect(process.env.MONGODB, { useNewUrlParser: true })
+.then(function() { console.log("MongoDB online on port 27017") })
+.catch(function(err) { console.log(err) });
+
+db.set("useCreateIndex", true);
+
+
+
+// http.createServer(app).listen(port); 
